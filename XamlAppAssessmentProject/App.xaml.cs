f10b4ccc5e0469620c6fb181d6590a3961870f75
JOHNLAPTOP1\kenji <kenji.ogi@live.com>
2024-01-15 16:47:40 -0800

    Initial checkin.

---
+++ XamlAppAssessmentProject/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XamlAppAssessmentProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
